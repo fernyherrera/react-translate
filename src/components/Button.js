bd35cb413caa24a9cd25c6f28632f2ff659805e8
Fernando <fherrera85@gmail.com>
2018-12-23 15:27:27 -0800

    Create context provider

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import LanguageContent from '../context/LanguageContext';
 
 class Button extends React.Component {
+  static contextType = LanguageContent;
 
   render() {
+    const text = this.context === 'english' ? 'Submit': 'Voorleggen';
     return (
-      <button className="ui primary button">Submit</button>
+      <button className="ui primary button">{text}</button>
     );
   };
 }
--- src/components/Field.js
+++ src/components/Field.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import LanguageContext from '../context/LanguageContext';
 
 class Field extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
+    const text = this.context === 'english' ? 'Name' : 'Naam';
+
     return (
       <div className="ui field">
-        <label htmlFor="">Name</label>
+        <label htmlFor="">{text}</label>
         <input type="text"/>
       </div>
     );
